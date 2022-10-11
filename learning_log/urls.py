e3371716ea0ff3474bf473a0e02a26fc8f634c19
Bruner <bruner.expert@yandex.ru>
2022-10-11 20:06:56 +0300

    create views urls base and index

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from learning_logs.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
