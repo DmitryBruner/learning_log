685eba7f621888f63c134b7a72aec3a879235e11
Bruner <bruner.expert@yandex.ru>
2022-10-12 08:25:56 +0300

    create page topics and base

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
-from learning_logs.views import index
+from learning_logs.views import index, topics
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='Home'),
+    path('topics/', topics, name='Topics'), #мой кусок
 ]
